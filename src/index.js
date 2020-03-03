40e273f91bf7b14190ccffb05945c43dfcd49066
IAleks94 <a12344@mail.ru>
2020-03-03 13:36:11 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let count = 0;
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    for (i = 0; i < str.length; i++) {
+        i === bracketsConfig.length ? (i -= bracketsConfig.length) : i;
+        let [openBracket, clousBracket] = bracketsConfig[i];
+        let regExp = new RegExp(`\\${openBracket}\\${clousBracket}`,'g');
+        if (openBracket/1 && clousBracket/1) {
+          regExp = new RegExp(`${openBracket}${clousBracket}`,'g');
+        }
+        let strMatc = str.match(regExp);
+        if (strMatc) {
+            let newStr = str.replace(regExp, "");
+            if (newStr) {
+                return check(newStr, bracketsConfig);
+            } else {
+                return true;
+            }
+        } else {
+            count++;
+            if (count === bracketsConfig.length) {
+                return false;
+            }
+        }
+    }
+    return false;
+};
+
